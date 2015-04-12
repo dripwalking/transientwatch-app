7a864b48b81cb1ca311cb0e6a86d065aecd44638
Alexey Miroshkin <alexey.miroshkin@ru.ibm.com>
2015-04-12 22:51:12 +0300

    Integrated for device debug

--- index.ios.js
+++ index.ios.js
@@ -15,9 +15,10 @@ var {
   Image,
   ListView,
   ScrollView,
-  PushNotificationIOS,
+  SwitchIOS,
 } = React;
 
+//var RefreshableListView = require('react-native-refreshable-listview');
 
 var REQUEST_URL = 'http://transientwatch.mybluemix.net/feed';
 
@@ -32,6 +33,10 @@ var transientwatch = React.createClass({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
       loaded: false,
+      eventSwitchIsOn: false,
+      eventSwitchRegressionIsOn: true,
+      colorTrueSwitchIsOn: true,
+      colorFalseSwitchIsOn: false,
     };
   },
 
@@ -51,18 +56,25 @@ var transientwatch = React.createClass({
       .done();
   },
 
-
   render: function() {
     if (!this.state.loaded) {
       return this.renderLoadingView();
     }
 
     return (
+      <ScrollView horizontal={false} pagingEnabled={true}>
+      <View style={styles.head}>
+        <Text style={styles.titleHead}>Latest News</Text>
+      </View>
+      <View style={styles.filter}>
+
+      </View>
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderNews}
         style={styles.listView}
       />
+      </ScrollView>
     );
   },
 
@@ -85,7 +97,7 @@ var transientwatch = React.createClass({
       
     } 
     return (
-      <ScrollView horizontal={false} pagingEnabled={true}>
+      
       <View style={styles.container}>
         <View style={styles.circle} />
         <View style={styles.rightContainer}>
@@ -94,33 +106,11 @@ var transientwatch = React.createClass({
           <Text style={styles.subtext}>{item.Source}</Text>
         </View>
       </View>
-      </ScrollView>
+      
     );
   },
 
 });
-
-// class NotificationTransient extends React.Component {
-//   componentWillMount() {
-//     PushNotificationIOS.addEventListener('notification', this._onNotification);
-//   }
-
-//   componentWillUnmount() {
-//     PushNotificationIOS.removeEventListener('notification', this._onNotification);
-//   }
-
-
-//   _onNotification(notification) {
-//     AlertIOS.alert(
-//       'Notification Received',
-//       'Alert message: ' + notification.getMessage(),
-//       [{
-//         text: 'Dismiss',
-//         onPress: null,
-//       }]
-//     );
-//   }
-// }
 
 var styles = StyleSheet.create({
   container: {
@@ -130,6 +120,26 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
     padding: 5,
+  },
+  head: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9C939A',
+    padding: 5,
+    marginTop: 20,
+    paddingTop: 8,
+    color: '#FFFFFF'
+  },
+  filter: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 5,
+    color: '#FFFFFF'
   },
   circle: {
     borderRadius: 3, 
@@ -154,6 +164,13 @@ var styles = StyleSheet.create({
     textAlign: 'left',
     color: '#463C37',
   },
+  titleHead: {
+    fontSize: 16,
+    marginBottom: 8,
+    textAlign: 'left',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
   text: {
     fontSize: 11,
     color: '#9C939A',
@@ -166,7 +183,6 @@ var styles = StyleSheet.create({
     fontStyle: 'italic',
   },
   listView: {
-    paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
 });
